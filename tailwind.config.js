d454b26a7344666733ec143972527db13695be8a
ZaibLComrade <kmjahanzaib@gmail.com>
2023-08-20 02:56:50 +0600

    Added Banner

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+	  extend: {
+		  colors: {
+			  "custom-pink": "#E527B2",
+			  "custom-black": "#111",
+			  "custom-offWhite": "#F9F6E2",
+			  "custom-grey": "#1111110d"
+			}		
+	  },
+  },
+  plugins: [require("daisyui")],
+}
+
