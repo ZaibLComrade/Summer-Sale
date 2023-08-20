84e66fda84cf19abe002ae708eb65a8b64e08178
ZaibLComrade <kmjahanzaib@gmail.com>
2023-08-20 22:11:28 +0600

    Purchase Cart Functional

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,25 @@
-const cont = document.getElementById("cards-container");
-const container = cont.children[0].children[1];
-const cards = container.children;
-console.log(cards);
+// Getting all DOM outputs fields
+const totalOutput = document.getElementById("total-output");
+const discountOutput = document.getElementById("discount-output");
+const grandTotal = document.getElementById("grand-total-output");
+const cartEntry = document.getElementById("log-output")
 
-for(let card of cards) {
-	for(let i = 0; i < card.childElementCount; i++) {
-		console.log(card.children[i]);
-	}
+// Getting discount rate
+const discount = document.getElementById("discount-rate").innerText.split('% ')[0];
+
+function addToCart(card) {
+	const name = card.querySelector("h2").innerText;
+	const newEntry = document.createElement("li");
+	newEntry.innerText = name;
+	cartEntry.appendChild(newEntry);
+	document.getElementById("log-hr-line").style.display = "block";
+
+	const price = card.querySelector("span").innerText;
+	totalValue = totalOutput.innerText;
+	const totalOutputValue = parseFloat(totalValue) + parseFloat(price);	
+	const off = totalOutputValue * discount / 100;
+
+	totalOutput.innerText = totalOutputValue.toFixed(2);
+	discountOutput.innerText = off.toFixed(2);
+	grandTotal.innerText = (totalOutputValue - off).toFixed(2);
 }
